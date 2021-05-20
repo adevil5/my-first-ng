a0da1ebf010978af49ddd14a2fe3cd53ffe0424e
adevil5 <alex.devillier@gmail.com>
2021-05-20 18:52:18 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# my-first-ng
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/my-first-ng)
---
+++ my-second-ng/src/app/app.component.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'my-second-ng'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('my-second-ng');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('my-second-ng app is running!');
+  });
+});
